9b9b57a316059fffd7bd5f70ab35098c49bb6533
h.anjos <h.anjos@996fe77a-b68b-d9c5-0cb2-28642bf4066d>
2010-05-31 01:46:58 +0000

    Making the Converter annotation be documented and inherited.

--- trunk/transmuter/src/main/java/transmuter/Converter.java
+++ trunk/transmuter/src/main/java/transmuter/Converter.java
@@ -1,10 +1,14 @@
 package transmuter;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Documented
+@Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Converter {
