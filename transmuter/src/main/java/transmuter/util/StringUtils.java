42ab64e21bdc5aba7327b890b267830ca8ea8686
Humberto Anjos <h.anjos@acm.org>
2010-06-21 22:14:30 +0000

    Removing unused utility methods.

--- transmuter/src/main/java/transmuter/util/StringUtils.java
+++ transmuter/src/main/java/transmuter/util/StringUtils.java
@@ -1,58 +1,24 @@
 package transmuter.util;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
+import static transmuter.util.ObjectUtils.*;
 
 public final class StringUtils {
   // ensuring non-instantiability and non-inheritability
   private StringUtils() { /* empty block */ }
 
-  public static interface Stringifier<T> {
-    public String stringify(T o);
-  }
-  
-  public static final Stringifier<Object> DEFAULT;
-  
-  static {
-    DEFAULT = new Stringifier<Object>() {
-      @Override
-      public String stringify(Object o) {
-        return String.valueOf(o);
-      }
-    };
-  }
-  
-  public static <T> String concatenate(String delim, T[] objects) {
-    return concatenate(DEFAULT, delim, objects);
-  }
-  
-  @SuppressWarnings("unchecked")
-  public static <T> String concatenate(Stringifier<? super T> stringifier, String delim, T[] objects) {
-    return concatenate(
-        stringifier, 
-        delim, 
-        (List<T>) ((objects != null) ? Arrays.asList(objects) : Collections.EMPTY_LIST));
-  }
-  
-  public static <T> String concatenate(String delim, Collection<T> objects) {
-    return concatenate(DEFAULT, delim, objects);
-  }
-  
-  public static <T> String concatenate(Stringifier<? super T> stringifier, String delim, Collection<T> objects) {
-    if (objects == null || objects.isEmpty())
+  public static String concatenate(String delim, Object... objects) {
+    if(isEmpty(objects))
       return "";
     
-    if(stringifier == null)
-      stringifier = DEFAULT;
-  
-    Iterator<T> iterator = objects.iterator();
-    StringBuilder sb = new StringBuilder(stringifier.stringify(iterator.next()));
+    String firstObjectAsString = String.valueOf(objects[0]);
     
-    while(iterator.hasNext())
-      sb.append(delim).append(stringifier.stringify(iterator.next()));
+    if(objects.length == 1)
+      return firstObjectAsString;
+    
+    delim = delim != null ? delim : "";
+    StringBuilder sb = new StringBuilder(firstObjectAsString);
+    for(int i = 1; i < objects.length; i++)
+      sb.append(delim).append(String.valueOf(objects[i]));
     
     return sb.toString();
   }
--- transmuter/src/test/java/transmuter/util/StringUtilsTest.java
+++ transmuter/src/test/java/transmuter/util/StringUtilsTest.java
@@ -3,41 +3,20 @@ package transmuter.util;
 import static org.junit.Assert.assertEquals;
 import static transmuter.util.StringUtils.concatenate;
 
-import java.lang.reflect.Type;
-import java.util.Arrays;
-import java.util.List;
-
 import org.junit.Test;
 
-import transmuter.type.TypeToken;
-import transmuter.util.StringUtils.Stringifier;
-
 public class StringUtilsTest {
-  @SuppressWarnings("unchecked")
   @Test
   public void testConcatenateWithoutAndWithNullStringifier() {
+    assertEquals("[a, b, c]", concatenate(", ", "[a", "b", "c]"));
     assertEquals("[a, b, c]", concatenate(", ", new Object[] { "[a", "b", "c]" }));
-    assertEquals("[a, b, c]", concatenate(null, ", ", new Object[] { "[a", "b", "c]" }));
-    assertEquals("double, class java.lang.String, null", concatenate(", ", Arrays.asList(double.class, String.class, null)));
-    assertEquals("double, class java.lang.String, null", concatenate(null, ", ", Arrays.asList(double.class, String.class, null)));
+    assertEquals("[abc]", concatenate(null, new Object[] { "[a", "b", "c]" }));
+    assertEquals("[abc]", concatenate("", new Object[] { "[a", "b", "c]" }));
+    assertEquals("double, class java.lang.String, null", concatenate(", ", double.class, String.class, null));
     
-    assertEquals("", concatenate(", ", new String[] {}));
+    assertEquals("", concatenate(", "));
+    assertEquals("", concatenate(null));
+    assertEquals("", concatenate(", ", new Object[] {}));
     assertEquals("", concatenate(", ", (Object[]) null));
-  }
-  
-  @Test
-  public void testConcatenateWithStringifier() {
-    assertEquals(
-        "double java.lang.String[] null java.util.List<java.lang.String>",
-        concatenate(new Stringifier<Type>() {
-          @Override
-          public String stringify(Type o) {
-            if(o == null)
-              return "null";
-            
-            return ReflectionUtils.getTypeName(o);
-          }},
-          " ", 
-          Arrays.asList(double.class, String[].class, null, new TypeToken<List<String>>() {}.getType())));
   }
 }
