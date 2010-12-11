7ecc1963bad688b8221f510d77850ddc12c02f4d
h.anjos <h.anjos@996fe77a-b68b-d9c5-0cb2-28642bf4066d>
2010-12-11 00:44:57 +0000

    release: copy for tag transmuter-0.1

---
+++ tags/transmuter-0.1/transmuter/src/main/java/com/googlecode/transmuter/exception/MultipleCausesException.java
@@ -0,0 +1,70 @@
+package com.googlecode.transmuter.exception;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * The superclass of exceptions which wrap a list of exceptions. Meant to be used when an operation may fail for 
+ * multiple reasons, and one wishes to report them all instead of only the first detected. 
+ * 
+ *  @author Humberto S. N. dos Anjos
+ */
+public class MultipleCausesException extends RuntimeException {
+  private static final long serialVersionUID = 1L;
+  
+  private static final List<Exception> EMPTY_EXCEPTION_LIST = Arrays.asList(new Exception[0]);
+  
+  private static String buildMessage(List<? extends Exception> causes) {
+    if(causes.isEmpty())
+      return "";
+    
+    StringBuilder sb = new StringBuilder("Multiple exceptions found:\n    ").append(causes.get(0));
+    
+    final int length = causes.size();
+    for(int i = 1; i < length; i++)
+      sb.append(";\n    ").append(causes.get(i));
+    
+    return sb.toString();
+  }
+
+  private List<? extends Exception> causes;
+  
+  /**
+   * Builds a new instance.
+   * 
+   * @param causes the exceptions to be bundled.
+   */
+  public MultipleCausesException(Exception... causes) {
+    this((causes != null) ? Arrays.asList(causes) : EMPTY_EXCEPTION_LIST);
+  }
+  
+  /**
+   * Builds a new instance.
+   * 
+   * @param causes the exceptions to be bundled.
+   */
+  public MultipleCausesException(List<? extends Exception> causes) {
+    super(buildMessage((causes != null) ? causes : EMPTY_EXCEPTION_LIST));
+    
+    this.causes = Collections.unmodifiableList((causes != null) ? causes : EMPTY_EXCEPTION_LIST);
+  }
+
+  /**
+   * Returns the bundled exceptions.
+   * 
+   * @return the bundled exceptions.
+   */
+  public List<? extends Exception> getCauses() {
+    return causes;
+  }
+  
+  /**
+   * Returns the first bundled exception, or {@code null} if none were given.
+   * @return the first bundled exception, or {@code null} if none were given.
+   */
+  @Override
+  public Throwable getCause() {
+    return causes.isEmpty() ? null : causes.get(0); 
+  }
+}
---
+++ tags/transmuter-0.1/transmuter/src/main/java/com/googlecode/transmuter/util/StringUtils.java
@@ -0,0 +1,49 @@
+package com.googlecode.transmuter.util;
+
+import static com.googlecode.transmuter.util.ObjectUtils.*;
+
+/**
+ * Some utility methods for Strings. This class cannot be inherited from or instantiated.
+ * 
+ * @author Humberto S. N. dos Anjos
+ */
+public final class StringUtils {
+  // ensuring non-instantiability and non-inheritability
+  private StringUtils() { /* empty block */ }
+
+  /**
+   * Returns a string holding the objects' string representation joined by the given delimiter.
+   * 
+   * Examples:
+   * <pre>
+   * concatenate(", ", new Object[] { "a", "b", "c" })     // --> "a, b, c"
+   * concatenate(", ", new Object[] { false, "b", "c" })   // --> "false, b, c"
+   * concatenate("",   new Object[] { "a", 1, "c" })       // --> "a1c"
+   * concatenate(null, new Object[] { "a", 1, "c" })       // --> "a1c"
+   * concatenate("//", new Object[] { "usr", "bin" })      // --> "usr//bin"
+   * concatenate("/",  new Object[] {}) // no arguments given --> ""
+   * concatenate("/",  null)            // no arguments given --> ""
+   * </pre>
+   * 
+   * @param delim the delimiter. Will be the empty string if it receives {@code null}.
+   * @param objects the given objects for concatenation.
+   * @return a string holding the objects' string representation joined by the given delimiter. Will be the empty 
+   * string if no objects are given.  
+   */
+  public static String concatenate(String delim, Object[] objects) {
+    if(isEmpty(objects))
+      return "";
+    
+    String firstObjectAsString = String.valueOf(objects[0]);
+    
+    if(objects.length == 1)
+      return firstObjectAsString;
+    
+    delim = delim != null ? delim : "";
+    StringBuilder sb = new StringBuilder(firstObjectAsString);
+    for(int i = 1; i < objects.length; i++)
+      sb.append(delim).append(String.valueOf(objects[i]));
+    
+    return sb.toString();
+  }
+}
