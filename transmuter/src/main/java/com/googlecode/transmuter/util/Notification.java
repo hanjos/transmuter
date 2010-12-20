c041c7abd3c27239edc4c8222db8c09fd7bfb8d9
Humberto Anjos <h.anjos@acm.org>
2010-12-20 11:48:44 +0000

    Removing Notification's useless constructor, and ensuring errors cannot be modified externally save for the add operation (former report()).

--- transmuter/src/main/java/com/googlecode/transmuter/util/Notification.java
+++ transmuter/src/main/java/com/googlecode/transmuter/util/Notification.java
@@ -17,26 +17,16 @@ public class Notification implements Iterable<Exception> {
   private static final List<Exception> EMPTY_LIST = Collections.emptyList();
   
   private List<Exception> errors;
+  private List<Exception> unmodifiableErrors; // since Collections.unmodifiableList is backed by the actual instance,
+                                              // we need only to calculate it once.
 
   // constructors
   /**
    * Builds a new Notification instance with no reported errors.
    */
   public Notification() {
-    this(null);
-  }
-  
-  /**
-   * Builds a new Notification instance which copies all reported errors from the given notification.
-   * 
-   * @param n a previously built notification. If {@code null}, no errors are inherited, and this instance 
-   * starts fresh.
-   */
-  public Notification(Notification n) {
-    if(n != null)
-      errors = new ArrayList<Exception>(n.errors);
-    else
-      errors = new ArrayList<Exception>();
+    errors = new ArrayList<Exception>();
+    unmodifiableErrors = Collections.unmodifiableList(errors);
   }
 
   // operations
@@ -46,8 +36,8 @@ public class Notification implements Iterable<Exception> {
    * @param _errors the reported errors. 
    * @return this instance.
    */
-  public Notification report(Exception... _errors) {
-    return report((_errors != null) ? Arrays.asList(_errors) : EMPTY_LIST);
+  public Notification add(Exception... _errors) {
+    return add((_errors != null) ? Arrays.asList(_errors) : EMPTY_LIST);
   }
   
   /**
@@ -56,7 +46,7 @@ public class Notification implements Iterable<Exception> {
    * @param _errors an iterable object holding the reported errors. 
    * @return this instance.
    */
-  public Notification report(Iterable<? extends Exception> _errors) {
+  public Notification add(Iterable<? extends Exception> _errors) {
     if(_errors == null)
       return this;
     
@@ -68,13 +58,14 @@ public class Notification implements Iterable<Exception> {
   }
   
   /**
-   * Returns an iterator for transversal of all the errors present in this instance.
+   * Returns an iterator for transversal of all the errors present in this instance. The iterator is unmodifiable, so
+   * one cannot alter this instance's internal records with it.
    * 
    * @return an iterator for transversal of all the errors present in this instance.
    */
   @Override
   public Iterator<Exception> iterator() {
-    return errors.iterator();
+    return getErrors().iterator();
   }
   
   // properties
@@ -97,11 +88,12 @@ public class Notification implements Iterable<Exception> {
   }
   
   /**
-   * Returns a list of all errors reported to this instance.
+   * Returns an unmodifiable list of all errors reported to this instance. The returned list is backed by the internal
+   * one, so additional errors reported in this notification will show up in the returned list.
    * 
-   * @return a list of all errors reported to this instance.
+   * @return an unmodifiable list of all errors reported to this instance.
    */
   public List<Exception> getErrors() {
-    return Collections.unmodifiableList(errors);
+    return unmodifiableErrors;
   }
 }
--- transmuter/src/test/java/com/googlecode/transmuter/util/NotificationTest.java
+++ transmuter/src/test/java/com/googlecode/transmuter/util/NotificationTest.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 import org.junit.Before;
@@ -35,13 +36,13 @@ public class NotificationTest {
     assertFalse(n.hasErrors());
     assertTrue(n.getErrors().isEmpty());
     
-    assertEquals(n, n.report(iae));
+    assertEquals(n, n.add(iae));
     
     assertFalse(n.isOk());
     assertTrue(n.hasErrors());
     assertArrayEquals(new Object[] { iae }, n.getErrors().toArray());
     
-    assertEquals(n, n.report(npe));
+    assertEquals(n, n.add(npe));
     
     assertFalse(n.isOk());
     assertTrue(n.hasErrors());
@@ -54,7 +55,7 @@ public class NotificationTest {
     assertFalse(n.hasErrors());
     assertTrue(n.getErrors().isEmpty());
     
-    assertEquals(n, n.report(Arrays.asList(iae, npe, e)));
+    assertEquals(n, n.add(Arrays.asList(iae, npe, e)));
     
     assertFalse(n.isOk());
     assertTrue(n.hasErrors());
@@ -67,7 +68,7 @@ public class NotificationTest {
     assertFalse(n.hasErrors());
     assertTrue(n.getErrors().isEmpty());
     
-    assertEquals(n, n.report(iae, npe, e));
+    assertEquals(n, n.add(iae, npe, e));
     
     assertFalse(n.isOk());
     assertTrue(n.hasErrors());
@@ -80,37 +81,37 @@ public class NotificationTest {
     assertFalse(n.hasErrors());
     assertTrue(n.getErrors().isEmpty());
     
-    assertEquals(n, n.report());
+    assertEquals(n, n.add());
     
     assertTrue(n.isOk());
     assertFalse(n.hasErrors());
     assertTrue(n.getErrors().isEmpty());
     
-    assertEquals(n, n.report((Exception) null));
+    assertEquals(n, n.add((Exception) null));
     
     assertTrue(n.isOk());
     assertFalse(n.hasErrors());
     assertTrue(n.getErrors().isEmpty());
     
-    assertEquals(n, n.report((Exception[]) null));
+    assertEquals(n, n.add((Exception[]) null));
     
     assertTrue(n.isOk());
     assertFalse(n.hasErrors());
     assertTrue(n.getErrors().isEmpty());
     
-    assertEquals(n, n.report(null, null, null));
+    assertEquals(n, n.add(null, null, null));
     
     assertTrue(n.isOk());
     assertFalse(n.hasErrors());
     assertTrue(n.getErrors().isEmpty());
     
-    assertEquals(n, n.report((Iterable<Exception>) null));
+    assertEquals(n, n.add((Iterable<Exception>) null));
     
     assertTrue(n.isOk());
     assertFalse(n.hasErrors());
     assertTrue(n.getErrors().isEmpty());
     
-    assertEquals(n, n.report(null, null, iae, null, null, null));
+    assertEquals(n, n.add(null, null, iae, null, null, null));
     
     assertFalse(n.isOk());
     assertTrue(n.hasErrors());
@@ -119,7 +120,7 @@ public class NotificationTest {
   
   @Test
   public void methodChaining() {
-    assertEquals(n, n.report(iae).report(null, null).report(npe).report(e));
+    assertEquals(n, n.add(iae).add(null, null).add(npe).add(e));
     assertArrayEquals(new Object[] { iae, npe, e }, n.getErrors().toArray());
   }
   
@@ -128,7 +129,7 @@ public class NotificationTest {
     for(@SuppressWarnings("unused") Exception error : n)
       fail("There should be no errors in n!");
     
-    assertEquals(n, n.report(iae, npe).report(e));
+    assertEquals(n, n.add(iae, npe).add(e));
     
     List<Exception> accumulated = new ArrayList<Exception>();
     for(Exception error : n)
@@ -144,19 +145,48 @@ public class NotificationTest {
     assertFalse(emptyN.hasErrors());
     assertTrue(emptyN.getErrors().isEmpty());
     
-    n.report(iae);
-    n.report(npe);
+    n.add(iae);
+    n.add(npe);
     
-    Notification heir = new Notification(n); // to you, son, I leave all my debts...
+    Notification heir = new Notification().add(n); // to you, son, I leave all my debts...
     assertFalse(heir.isOk());
     assertTrue(heir.hasErrors());
     assertArrayEquals(new Object[] { iae, npe }, heir.getErrors().toArray());
     assertArrayEquals(n.getErrors().toArray(), heir.getErrors().toArray());
     
-    heir.report(e);
+    heir.add(e);
     assertFalse(heir.isOk());
     assertTrue(heir.hasErrors());
     assertArrayEquals(new Object[] { iae, npe, e }, heir.getErrors().toArray());
     assertArrayEquals(new Object[] { iae, npe }, n.getErrors().toArray());
+  }
+  
+  @Test
+  public void getErrors() {
+    n.add(iae, npe);
+    
+    List<Exception> errors = n.getErrors();
+    
+    assertTrue(errors.contains(iae));
+    assertTrue(errors.contains(npe));
+    assertFalse(errors.contains(e));
+    
+    try {
+      errors.add(e);
+      fail();
+    } catch(UnsupportedOperationException e) {
+      // expected; we can safely ignore it
+    }
+    
+    n.add(e);
+    assertTrue(errors.contains(e));
+    
+    Iterator<Exception> iterator = n.iterator();
+    
+    try {
+      iterator.remove();
+    } catch(UnsupportedOperationException e) {
+      // expected; we can safely ignore it
+    }
   }
 }
