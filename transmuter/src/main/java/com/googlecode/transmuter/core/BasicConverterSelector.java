ae9e18af91fff63e93a7a27a1c220dc8a8c952ba
Humberto Anjos <h.anjos@acm.org>
2011-03-27 22:48:46 +0000

    Adding a getConverterFor method which takes a Map.

--- transmuter/src/main/java/com/googlecode/transmuter/core/BasicConverterSelector.java
+++ transmuter/src/main/java/com/googlecode/transmuter/core/BasicConverterSelector.java
@@ -1,7 +1,10 @@
 package com.googlecode.transmuter.core;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import com.googlecode.transmuter.converter.Converter;
 import com.googlecode.transmuter.converter.ConverterType;
@@ -54,6 +57,51 @@ public class BasicConverterSelector implements ConverterSelector {
     
     if(compatibles.isEmpty()) // no compatibles found, blow up
       throw new NoCompatibleConvertersFoundException(type, CollectionUtils.toList(converters));
+    
+    // lots of compatibles found, how to pick only one?
+    throw new TooManyConvertersFoundException(type, compatibles);
+  }
+
+  /**
+   * Does a simple search in the given converter map for a type match. 
+   * <p>
+   * If an exact match is found, its converter is returned; if not, a compatible converter will be searched for. 
+   * Exceptions are thrown if no compatible converter is found, or more than one is found, since this implementation
+   * does not know which to choose.
+   * 
+   * @param type the type to match.
+   * @param map where to look for a matching converter.
+   * @return a converter with the same type or a compatible one.
+   * @throws NoCompatibleConvertersFoundException if no converters compatible with the given type were found.
+   * @throws TooManyConvertersFoundException if no exact match for the given type is found, and there is more than one 
+   *         compatible converter.
+   */
+  @SuppressWarnings("unchecked")
+  @Override
+  public Converter getConverterFor(ConverterType type, Map<? extends ConverterType, ? extends Converter> map)
+      throws NoCompatibleConvertersFoundException, TooManyConvertersFoundException {
+    if(type == null || map == null || map.isEmpty())
+      throw new NoCompatibleConvertersFoundException(type, Collections.EMPTY_LIST);
+    
+    if(map.containsKey(type)) // found a perfect match!
+      return map.get(type);
+    
+    List<Converter> compatibles = new ArrayList<Converter>();
+    for(Entry<? extends ConverterType, ? extends Converter> entry : map.entrySet()) {
+      if(entry.getKey() == null)
+        continue;
+      
+      if(entry.getKey().isAssignableFrom(type)) { // this may do
+        compatibles.add(entry.getValue());
+        continue;
+      }
+    }
+    
+    if(compatibles.size() == 1) // found only one compatible, use it
+      return compatibles.get(0);
+    
+    if(compatibles.isEmpty()) // no compatibles found, blow up
+      throw new NoCompatibleConvertersFoundException(type, map.values());
     
     // lots of compatibles found, how to pick only one?
     throw new TooManyConvertersFoundException(type, compatibles);
--- transmuter/src/main/java/com/googlecode/transmuter/core/ConverterSelector.java
+++ transmuter/src/main/java/com/googlecode/transmuter/core/ConverterSelector.java
@@ -1,5 +1,7 @@
 package com.googlecode.transmuter.core;
 
+import java.util.Map;
+
 import com.googlecode.transmuter.converter.Converter;
 import com.googlecode.transmuter.converter.ConverterType;
 import com.googlecode.transmuter.core.exception.NoCompatibleConvertersFoundException;
@@ -28,5 +30,23 @@ public interface ConverterSelector {
    * to decide which should be picked.
    */
   Converter getConverterFor(ConverterType type, Iterable<? extends Converter> converters) 
+      throws NoCompatibleConvertersFoundException, TooManyConvertersFoundException;
+  
+  /**
+   * Searches for a converter which either exactly matches or is compatible with the given converter type using 
+   * the given converter map. 
+   * <p>
+   * Lacking an exact match, a compatible converter will be searched for. An exception will be thrown if no converter 
+   * is found, or if more than one compatible (non-exact match) converter is found and this selector cannot decide 
+   * which should be returned.
+   * 
+   * @param type a converter type.
+   * @param map where to look for a matching converter.
+   * @return a converter compatible with the given converter type. May not an exact match.
+   * @throws NoCompatibleConvertersFoundException no compatible converters were found.
+   * @throws TooManyConvertersFoundException more than one compatible converter was found, and this selector was unable
+   * to decide which should be picked.
+   */
+  Converter getConverterFor(ConverterType type, Map<? extends ConverterType, ? extends Converter> map) 
       throws NoCompatibleConvertersFoundException, TooManyConvertersFoundException;
 }
